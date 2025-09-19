96c567969750ec4992af60713a762380ffc5994b
Smeetcodes <smeetyadao76@gmail.com>
2025-09-19 17:08:54 +0530

    task4

---
+++ querySelector & querySelectorAll Methods/querySelector & querySelectorAll Methods.js
@@ -0,0 +1,18 @@
+// Write the code as shown in the video below:
+
+// Write answer to the questions asked below:
+const mainHeading = document.querySelector('#main-heading');
+mainHeading.style.textAlign = 'right';
+
+const basket = document.querySelector('h2');
+basket.style.marginLeft = '30px';
+basket.style.color = 'brown';
+
+const fruit = document.querySelectorAll('.fruit');
+
+fruit[1].style.backgroundColor = 'brown';
+fruit[1].style.color = 'white';
+
+fruit[3].style.backgroundColor = 'brown';
+fruit[3].style.color = 'white';
+
